b66cd37dcd677c8570bfcbfb2277a9be0b495634
Joel Francisco <joel.francisco@zenn.pt>
2018-04-23 20:11:20 +0100

    PropertyChanged

--- Application.Core/Entities/Abstract/BaseEntity.cs
+++ Application.Core/Entities/Abstract/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,59 @@ namespace Application.Core.Entities
 {
     public abstract class BaseEntity : IEntity
     {
-        public int Id { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime? DateUpdated { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected int _id;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Id)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsNew)));
+                }
+            }
+        }
+
+        protected DateTime _dateCreated;
+        public DateTime DateCreated
+        {
+            get
+            {
+                return _dateCreated;
+            }
+            set
+            {
+                if (value != _dateCreated)
+                {
+                    _dateCreated = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateCreated)));
+                }
+            }
+        }
+
+        protected DateTime? _dateUpdated;
+        public DateTime? DateUpdated
+        {
+            get
+            {
+                return _dateUpdated;
+            }
+            set
+            {
+                if (value != _dateUpdated)
+                {
+                    _dateUpdated = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateUpdated)));
+                }
+            }
+        }
 
         public bool IsNew
         {
--- Application.Core/Entities/Abstract/IEntity.cs
+++ Application.Core/Entities/Abstract/IEntity.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace Application.Core.Entities
 {
-    public interface IEntity
+    public interface IEntity : INotifyPropertyChanged
     {
         int Id { get; set; }
         DateTime DateCreated { get; set; }
